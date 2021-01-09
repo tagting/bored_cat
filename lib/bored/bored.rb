09b4eacbd635ef739e1b2ad8a9fcd31b7df25ead
ting <ttzhenng@gmail.com>
2021-01-09 02:21:55 -0500

    setup bored.rb

---
+++ lib/bored/bored.rb
@@ -0,0 +1,27 @@
+#What do you want you object model to look like
+
+class Bored
+
+    @@all = []
+
+    attr_accessor :activity, :accessibility, :type, :participants, :price, :link, :key 
+
+    def initialize(activity, accessibility, type, participants, price, link, key)
+        @activity = activity
+        @accessibility = accessibility
+        @type = type
+        @participants = participants
+        @price = price
+        @link = link
+        @key = key
+        save
+    end
+
+    def save
+        @@all <<self
+    end
+
+    def self.all #to read the array
+        @@all
+    end
+end
